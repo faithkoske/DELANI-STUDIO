6154e72fb98bcd791fedd2970fa3e62673f94258
faithkoske <chemutaifaith10@gmail.com>
2019-06-10 12:41:36 +0300

    add file changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,38 @@
-$(document).ready(function() {
-  $(".clickable1").click(function() {
-    $(".design-hidden").toggle();
-    $(".design-showing").toggle();
+$(document).ready(function(){
+  $(".state1").click(function(){
+      $(".state1").hide();
+      $(".design").show();
+      $(".arm1").toggle();
   });
-  $(document).ready(function() {
-  $(".clickable2").click(function() {
-    $(".development-hidden").toggle();
-    $(".development-showing").toggle();
+
+  $(".arm1").click(function(){
+      $(".state1").show();
+      $(".arm1").toggle();
   });
-});
-$(document).ready(function() {
-$(".clickable3").click(function() {
-  $(".product-hidden").toggle();
-  $(".product-showing").toggle();
-});
+
+  
+
+  $("#state2").click(function(){
+      $("#state2").hide();
+      $("#development").show();
+      $(".arm2").toggle();
+  });
+
+  $(".arm2").click(function(){
+      $("#state2").show();
+      $(".arm2").toggle();
+  });
+
+  
+
+  $("#state3").click(function(){
+      $("#state3").hide();
+      $("#development").show();
+      $(".arm3").toggle();
+  });
+
+  $(".arm3").click(function(){
+      $("#state3").show();
+      $(".arm3").toggle();
+  });
 });
